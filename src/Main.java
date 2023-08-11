df9c8999377fd013ed8698747be9b1d6d2f6a7ba
eujoaolima <joaopedro.donatolima@hotmail.com>
2023-08-11 14:26:20 -0300

    Gameboard

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
+import Entities.Position;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
     }
 }
