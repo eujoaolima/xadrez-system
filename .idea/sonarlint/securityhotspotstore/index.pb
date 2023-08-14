730ea01ad62ff76487a45b5a19218471516c99e3
eujoaolima <joaopedro.donatolima@hotmail.com>
2023-08-14 08:14:59 -0300

    Implementação dos movimentos do Rei

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,13 @@
+
+D
+src/Enums/Color.java,f\8\f86a3903702a4d7f7b671272393a33705fe93d43
+Z
+*src/Entities/Boardgame/BoardException.java,c\4\c48f1ba68863c2c511a66bbfbe17f7ed62673001
+Q
+!src/Entities/Boardgame/Board.java,2\0\208278ff9559873f7f0ca8208436547abbb141a4
+V
+&src/Entities/Chess/ChessException.java,d\d\dd2d141c7b3ebade92c2b58f19ddc4f254cf79aa
+U
+%src/Entities/Chess/ChessPosition.java,8\8\888777d3c4e2a1f7bbee271c0b4a615382884ac3
+Q
+!src/Entities/Boardgame/Piece.java,3\d\3d0b970e54e0db705f8b4ba928f7ec3180917c27
--- src/Entities/Pieces/King.java
+++ src/Entities/Pieces/King.java
@@ -1,6 +1,7 @@
 package Entities.Pieces;
 
 import Entities.Boardgame.Board;
+import Entities.Boardgame.Position;
 import Entities.Chess.ChessPiece;
 import Enums.Color;
 
@@ -14,9 +15,73 @@ public class King extends ChessPiece {
         return "K";
     }
 
+    private boolean canMove(Position position) {
+        ChessPiece p = (ChessPiece) getBoard().piece(position);
+        return p == null || p.getColor() != getColor();
+    }
+
     @Override
     public boolean[][] possibleMoves() {
         boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getCols()];
-        return new boolean[0][];
+
+        Position p = new Position(0, 0);
+
+        // Acima
+
+        p.setValues(position.getRow() - 1, position.getCol());
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getCol()] = true;
+        }
+
+        // Abaixo
+
+        p.setValues(position.getRow() + 1, position.getCol());
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getCol()] = true;
+        }
+
+        // Esquerda
+
+        p.setValues(position.getRow(), position.getCol()  - 1);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getCol()] = true;
+        }
+
+        // Direta
+
+        p.setValues(position.getRow(), position.getCol()  + 1);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getCol()] = true;
+        }
+
+        // Cima-Esquerda
+
+        p.setValues(position.getRow() - 1 , position.getCol()  - 1);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getCol()] = true;
+        }
+
+        // Cima-Direita
+
+        p.setValues(position.getRow() - 1, position.getCol()  + 1);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getCol()] = true;
+        }
+
+        // Baixo-Esquerda
+
+        p.setValues(position.getRow() + 1, position.getCol()  - 1);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getCol()] = true;
+        }
+
+        // Baixo-Direita
+
+        p.setValues(position.getRow() + 1, position.getCol() + 1);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getCol()] = true;
+        }
+
+        return mat;
     }
 }
